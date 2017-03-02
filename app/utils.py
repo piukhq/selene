e8a688d72666fac71fb94f8e35db269775213482
olivere <oe@bink.com>
2017-03-02 09:11:31 +0000

    Updated regex for post code validation.

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,4 @@
-import requests
+import re
 import importlib
 
 from app.active import AGENTS
@@ -10,26 +10,16 @@ def resolve_agent(name):
     module = importlib.import_module('app.agents.{}'.format(module_name))
     return getattr(module, class_name)
 
-
 def validate_uk_postcode(postcode):
-    """Validate a UK post code"""
+    # validate post code using regex
+    pattern = re.compile('^[A-Z]{2}[0-9][A-Z] *?[0-9][A-Z]{2}$'
+                         '|^[A-Z][0-9][A-Z] *?[0-9][A-Z]{2}$'
+                         '|^[A-Z][0-9] *?[0-9][A-Z]{2}$'
+                         '|^[A-Z][0-9]{2} *?[0-9][A-Z]{2}$'
+                         '|^[A-Z]{2}[0-9] *?[0-9][A-Z]{2}$'
+                         '|^[A-Z]{2}[0-9]{2} *?[0-9][A-Z]{2}$')
 
-    # old API key: KLogDsJB7E2Oy-2WHME-tQ2451
-    result = False
-    if len(postcode):
-        r = requests.get('https://api.getAddress.io/v2/uk/{}?api-key=1h-2FYKur0-3JmtmPmNlRQ7622'.format(postcode))
+    if not re.match(pattern, postcode):
+        return False
 
-        if r.status_code == 200:
-            try:
-                result = bool(r.json()['Addresses'])
-            except KeyError:
-                print("EXCEPTION: KeyError 'Addresses' for post code {}".format(postcode))
-        else:
-            print('Got HTTP status code {} from third party post code validator for post code {}.'.format(r.status_code, postcode))
-
-    if result:
-        print('Post code {} is valid'.format(postcode))
-    else:
-        print('Post code {} is invalid'.format(postcode))
-
-    return result
+    return True
