47beb6cb01a71785242bcf56ec0f80f39a2b7878
Francesco Milani <fmilani@bink.com>
2017-12-12 17:39:56 +0000

    remove old broken test

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ manager = Manager(app)
 manager.add_command("runserver", Server(port=settings.DEV_PORT, host=settings.DEV_HOST))
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-UNIT_TEST_PATH = os.path.join(HERE, 'app', 'tests', 'unit')
+UNIT_TEST_PATH = os.path.join(HERE, 'app', 'tests')
 
 
 @manager.command
