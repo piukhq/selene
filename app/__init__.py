b806ac6be519c385e24b3688e866a7ec8fd7fac2
Francesco Milani <fmilani@bink.com>
2017-12-13 09:24:21 +0000

    add merchants folder creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
+import os
+import settings
 from flask import Flask
 
 
 def create_app(config_name='settings'):
     from app.resources import api
+
+    path = os.path.join(settings.APP_DIR, 'merchants')
+
+    os.makedirs(path, exist_ok=True)
+    os.makedirs(path + '/visa', exist_ok=True)
+    os.makedirs(path + '/amex', exist_ok=True)
+    os.makedirs(path + '/mastercard', exist_ok=True)
 
     app = Flask('main')
     app.config.from_object(config_name)
