c0a08d25e7607e836670d4c50ff755afd5ef6c01
k-aziz <kaziz@bink.com>
2018-10-10 10:37:44 +0100

    Add unit tests for utils functions

--- app/tests/unit/test_utils.py
+++ app/tests/unit/test_utils.py
@@ -1,22 +1,11 @@
-import os
-import shutil
 import unittest
-import settings
+from unittest import mock
 
 from app import utils
+from app.utils import validate_headers, save_blob
 
 
 class TestUtils(unittest.TestCase):
-    path = os.path.join(settings.WRITE_FOLDER, 'app', 'tests', 'unit', 'test_dir')
-
-    def setUp(self):
-        os.makedirs(self.path, exist_ok=True)
-        self.old_write_folder = settings.WRITE_FOLDER
-        settings.WRITE_FOLDER = self.path
-
-    def tearDown(self):
-        shutil.rmtree(self.path)
-        settings.WRITE_FOLDER = self.old_write_folder
 
     def test_uk_postcode_validator(self):
         good_postcodes = [
@@ -43,6 +32,77 @@ class TestUtils(unittest.TestCase):
 
         for p in bad_postcodes:
             self.assertFalse(utils.validate_uk_postcode(p))
+
+    def test_validate_headers_success(self):
+        expected_headers = ['Partner Name', 'American Express MIDs', 'MasterCard MIDs', 'Visa MIDs',
+                            'Address (Building Name/Number, Street)', 'Postcode', 'Town/City', 'County/State',
+                            'Country', 'Action', 'Scheme', 'Scheme ID']
+
+        is_valid, invalid_headers, missing_headers = validate_headers(expected_headers)
+
+        self.assertTrue(is_valid)
+        self.assertEqual(invalid_headers, [])
+        self.assertEqual(missing_headers, [])
+
+    def test_validate_headers_invalid_headers(self):
+        extra_header = ['Partner Name', 'American Express MIDs', 'MasterCard MIDs', 'Visa MIDs',
+                        'Address (Building Name/Number, Street)', 'Postcode', 'Town/City', 'County/State',
+                        'Country', 'Action', 'Scheme', 'Scheme ID', 'Extra Column']
+
+        is_valid, invalid_headers, missing_headers = validate_headers(extra_header)
+
+        self.assertFalse(is_valid)
+        self.assertEqual(invalid_headers, ['Extra Column'])
+        self.assertEqual(missing_headers, [])
+
+    def test_validate_headers_missing_headers(self):
+        missing_header = ['American Express MIDs', 'MasterCard MIDs', 'Visa MIDs',
+                          'Address (Building Name/Number, Street)', 'Postcode', 'Town/City', 'County/State',
+                          'Country', 'Action']
+
+        is_valid, invalid_headers, missing_headers = validate_headers(missing_header)
+
+        self.assertFalse(is_valid)
+        self.assertEqual(invalid_headers, [])
+        self.assertEqual(missing_headers, ['Partner Name', 'Scheme', 'Scheme ID'])
+
+    @mock.patch('app.utils.bbs', autospec=True)
+    def test_save_blob_with_text_calls_correct_function(self, mock_blob_storage):
+        text = 'Text to store'
+        container = 'some-container'
+        filename = 'name.csv'
+        path = '/some_path'
+
+        save_blob(text, container, filename, content_type='text', path=path)
+
+        self.assertTrue(mock_blob_storage.create_blob_from_text.called)
+        self.assertFalse(mock_blob_storage.create_blob_from_bytes.called)
+
+        args, kwargs = mock_blob_storage.create_blob_from_text.call_args_list[0]
+        kwarg_tuples = kwargs.items()
+
+        self.assertIn(('blob_name', 'some_path/name.csv'), kwarg_tuples)
+        self.assertIn(('container_name', 'some-container'), kwarg_tuples)
+        self.assertIn(('text', 'Text to store'), kwarg_tuples)
+
+    @mock.patch('app.utils.bbs', autospec=True)
+    def test_save_blob_with_bytes_calls_correct_function(self, mock_blob_storage):
+        text = b''
+        container = 'some-container'
+        filename = 'name.csv'
+        path = '/some_path'
+
+        save_blob(text, container, filename, content_type='bytes', path=path)
+
+        self.assertTrue(mock_blob_storage.create_blob_from_bytes.called)
+        self.assertFalse(mock_blob_storage.create_blob_from_text.called)
+
+        args, kwargs = mock_blob_storage.create_blob_from_bytes.call_args_list[0]
+        kwarg_tuples = kwargs.items()
+
+        self.assertIn(('blob_name', 'some_path/name.csv'), kwarg_tuples)
+        self.assertIn(('container_name', 'some-container'), kwarg_tuples)
+        self.assertIn(('blob', b''), kwarg_tuples)
 
 
 if __name__ == '__main__':
